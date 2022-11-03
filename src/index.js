f76aaecd6e2fea59b4750c93a55441b2763dd2d5
Arthur Osipov <osartie@gmail.com>
2022-11-03 08:43:00 +0200

    Была изменена структура проекта. Был добавлен компонент списка дел TodoList

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bulma/css/bulma.css';
-import App from './App';
+import App from './components/app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
