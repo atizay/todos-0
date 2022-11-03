3a67b3812b4fdbe4babb30270178bb844c6cc1ed
Arthur Osipov <osartie@gmail.com>
2022-11-03 08:19:56 +0200

    Был добавлен код компонента App

--- src/App.js
+++ src/App.js
@@ -20,4 +20,29 @@ const initialData = [
     createdAt: date2.toLocaleString(),
     key: date2.getTime()
   }
-]
+];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.data = initialData;
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar is-light">
+          <div className="navbar-brand">
+            <span className="navbar-item is-uppercase">
+              Todos
+            </span>
+          </div>
+        </nav>
+        <main className="context px-6 mt-6">
+          <h1>Todos</h1>
+        </main>
+      </div>
+    )
+  }
+
+}
